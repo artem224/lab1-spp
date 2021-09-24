60cbff05d1ec9d469428925a872fea59370a2c6c
artem224 <72102981+artem224@users.noreply.github.com>
2021-09-24 14:42:55 +0300

    Add files via upload

---
+++ TracerLib/Tracers/MethodTracer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
+using MethodInfo = TracerLib.Models.MethodInfo;
+
+namespace TracerLib.Tracers
+{
+    public class MethodTracer
+    {
+        public Stopwatch Stopwatch { get; } = new ();
+        public ConcurrentStack<MethodInfo> Methods { get; } = new ();
+
+        public void AddMethod(MethodInfo methodInfo)
+        {
+            Methods.Push(methodInfo);
+        }
+    }
+}
---
+++ TracerLib/Tracers/ThreadTracer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using TracerLib.Models;
+
+namespace TracerLib.Tracers
+{
+    public class ThreadTracer
+    {
+        public ConcurrentStack<MethodTracer> MethodsTraces { get; init; } = new();
+        public ConcurrentStack<MethodInfo> MethodsInfo { get; init; } = new();
+        public MethodTracer CurrentMethodTracer { get; set; }
+
+        public void StartTrace()
+        {
+            if (CurrentMethodTracer is not null)
+            {
+                MethodsTraces.Push(CurrentMethodTracer);
+            }
+
+            CurrentMethodTracer = new MethodTracer();
+            CurrentMethodTracer.Stopwatch.Start();
+        }
+
+        public void StopTrace(StackFrame stackFrame)
+        {
+            CurrentMethodTracer.Stopwatch.Stop();
+            var methodInfo = new MethodInfo()
+            {
+                Name = stackFrame.GetMethod()?.Name,
+                Class = stackFrame.GetMethod()?.ReflectedType?.Name,
+                ElapsedMilliseconds = CurrentMethodTracer.Stopwatch.ElapsedMilliseconds,
+                Methods = CurrentMethodTracer.Methods.ToList()
+            };
+            
+            if (MethodsTraces.IsEmpty)
+            {
+                MethodsInfo.Push(methodInfo);
+                CurrentMethodTracer = null;
+            }
+            else
+            {
+                if (MethodsTraces.TryPop(out var methodTrace))
+                {
+                    CurrentMethodTracer = methodTrace;
+                    CurrentMethodTracer.AddMethod(methodInfo);
+                }
+            }
+        }
+    }
+}
